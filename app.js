6be8bb8ff9d56a856b659eefa8a287fbe35a2e10
Luciano Garcia <lugarsp@yahoo.com.br>
2025-03-30 18:28:53 -0300

    teste
    teste

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
-
+//teste
 // enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
     chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
